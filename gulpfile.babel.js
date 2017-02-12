b5c55df48e9c347455cecf57a45d32f806237031
Matias Verdier <verdier.matias@gmail.com>
2017-02-12 19:10:58 -0300

    Update path of css task to include subdirectories (#20)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -19,7 +19,7 @@ gulp.task("build", ["css", "js", "hugo"]);
 gulp.task("build-preview", ["css", "js", "hugo-preview"]);
 
 gulp.task("css", () => (
-  gulp.src("./src/css/*.css")
+  gulp.src("./src/css/**/*.css")
     .pipe(postcss([cssnext(), cssImport({from: "./src/css/main.css"})]))
     .pipe(gulp.dest("./dist/css"))
     .pipe(browserSync.stream())
