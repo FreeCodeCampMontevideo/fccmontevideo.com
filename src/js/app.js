28510b97280cd885571ea451c92b5c26ed491798
Matias Verdier <verdier.matias@gmail.com>
2017-02-26 19:43:41 -0300

    Fix scroll to anchor

--- src/js/app.js
+++ src/js/app.js
@@ -2,17 +2,19 @@
 
 
 /* global $ */
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") && location.hostname === this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-      if (target.length) {
-        $("html, body").animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+$(document).ready(function() {
+    var navigationHeight = $('.navigation').height();
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate({
+                    scrollTop: target.offset().top - navigationHeight
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
