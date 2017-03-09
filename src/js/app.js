81b4dc9473502d8aaad918dd7f67aa5b6332c49f
Matias Verdier <verdier.matias@gmail.com>
2017-03-09 02:23:25 -0300

    Fix indentation

--- src/js/app.js
+++ src/js/app.js
@@ -3,18 +3,18 @@
 
 /* global $ */
 $(document).ready(function() {
-    var navigationHeight = $('.navigation').height();
+  var navigationHeight = $(".navigation").height();
 
-    $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") && location.hostname === this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate({
-                    scrollTop: target.offset().top - navigationHeight
-                }, 1000);
-                return false;
-            }
-        }
-    });
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate({
+          scrollTop: target.offset().top - navigationHeight
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
