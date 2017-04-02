36a01090c8df31e408b75149e385f5e6c0c4b5cb
Matias Verdier <verdier.matias@gmail.com>
2017-04-02 01:29:14 -0300

    Preview logged in user as author

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -4,6 +4,8 @@ import format from "date-fns/format";
 export default class PostPreview extends React.Component {
   render() {
     const {entry, widgetFor} = this.props;
+    const stringUserData = localStorage.getItem("netlify-cms-user");
+    const user = stringUserData && JSON.parse(stringUserData);
 
     return (
       <div className="post">
@@ -11,16 +13,17 @@ export default class PostPreview extends React.Component {
           <h1>{ entry.getIn(["data", "title"])}</h1>
         </div>
 
-        <div className="post-meta">
-          <span className="post-author">
-             { entry.getIn(["data", "author"]) }
+        <div className="post-meta" style={{marginRight: 10}}>
+          <span className="post-author" style={{marginRight: 10}}>
+            <img className="author-avatar" style={{marginRight: 5}} src={user.avatar_url} alt="" />
+            { entry.getIn(["data", "author"]) }
           </span>
           -
-          <span className="post-date">
+          <span className="post-date" style={{margin: "0 10px"}}>
             { format(entry.getIn(["data", "date"]), "DD/MM/YYYY") }
           </span>
           -
-          <span className="read-time">Lectura x min</span>
+          <span className="read-time" style={{marginLeft: 10}}>Lectura x min</span>
         </div>
 
         <div className="post-content">
