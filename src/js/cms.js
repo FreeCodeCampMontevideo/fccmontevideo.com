8886ee5ca8916f06745cfe4d0154440bcba2a79c
Matias Verdier <verdier.matias@gmail.com>
2017-04-01 21:53:20 -0300

    Add css

---
+++ src/js/cms.js
@@ -0,0 +1,23 @@
+import React from "react";
+import CMS from "netlify-cms";
+
+// import PostPreview from "./cms-preview-templates/post";
+// import ProductsPreview from "./cms-preview-templates/products";
+
+
+// Example of creating a custom color widget
+class ColorControl extends React.Component {
+  render() {
+    return <input
+        style={{height: "80px"}}
+        type="color"
+        value={this.props.value}
+        onInput={(e) => this.props.onChange(e.target.value)}
+    />;
+  }
+}
+
+CMS.registerPreviewStyle("/css/main.css");
+// CMS.registerPreviewTemplate("post", PostPreview);
+// CMS.registerPreviewTemplate("products", ProductsPreview);
+CMS.registerWidget("color", ColorControl);
